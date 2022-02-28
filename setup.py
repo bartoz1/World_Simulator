ff4150c89ff50a9cf74f65f28561d2bf75ce86fa
bzylwis <nil112101@gmail.com>
2022-02-28 23:08:45 +0100

    including assets in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-   name='worldsimpulator',
+   name='worldsimulator',
    version='0.9',
    description='Simulator of the world in which there are plants and animals, including human with special ability.',
    author='RushKappa',
    author_email='bartoszzylwis@gmail.com',
-   package_data={'':['save.txt']},
+   package_data={'worldsimulator\\assets':['*.png', '*.bmp', '*.ttf', '*.otf']},
+   include_package_data=True,
    install_requires=required,
    packages=find_packages(),
    
